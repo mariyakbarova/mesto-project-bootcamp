9891300172c81e34ccc8dc3dae095063093008b4
Mariya Akbarova <nailmaru@yandex.ru>
2022-08-04 11:01:51 +0300

    feat: сделать удаление и лайк для фотографий

---
+++ skripts/index.js
@@ -0,0 +1,48 @@
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+const container = document.querySelector('.tape')
+
+container.addEventListener('click', function(e) {
+    if (e.target.className  === 'tape__delete') {
+        const listItem = e.target.closest('.tape__elements')
+        listItem.remove()
+    }
+})
+
+const likeButton = document.querySelector('.tape')
+
+container.addEventListener('click', function(evt) {
+    if (evt.target.classList.contains('tape__button')) {
+        evt.target.classList.toggle('tape__button_active')
+    }
+})
+
+
+
+
+
